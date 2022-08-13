82c69b58f8d8d10e4e45da8e01571c1e5ee8c42f
manjularamdoss <110729189+manjularamdoss@users.noreply.github.com>
2022-08-13 21:04:01 +0530

    Add files via upload

---
+++ week1.day1/Car.java
@@ -0,0 +1,18 @@
+package wee1.day1;
+
+public class Car {
+	
+	public void applyBreak() {
+System.out.println("Apply break");
+	}
+	public void soundHorn() {
+		System.out.println("Sound Horn");	
+		}
+	public static void main(String[] args) {
+		Car BikeClass =new Car();
+		BikeClass.applyBreak();
+		BikeClass.soundHorn();
+	}
+}
+
+
