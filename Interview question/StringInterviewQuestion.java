1e89e4e7fa8a05595929195cf11e2057e6089775
manjularamdoss <110729189+manjularamdoss@users.noreply.github.com>
2022-08-26 08:13:39 +0530

    Add files via upload

---
+++ Interview question/StringInterviewQuestion.java
@@ -0,0 +1,19 @@
+package week3.day2;
+
+public class StringInterviewQuestion {
+
+
+    public static void main(String[] args)
+    {
+        String input = "Name 1234";
+    	String[] word = input.split(" ", 2);
+    	String first = word[0];
+    	
+    	System.out.println("First word: " + first);
+    	
+    	
+}
+}
+
+
+
